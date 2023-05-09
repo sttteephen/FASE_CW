d68d12bd52d9d7886fde36a74584f73e91f75039
sttteephen <stepalmesse@gmail.com>
2023-05-09 08:19:43 +0100

    report

--- Coursework/src/spacestation.ads
+++ Coursework/src/spacestation.ads
@@ -1,106 +1,92 @@
 package SpaceStation with SPARK_Mode is
    
    type AirLockStatus is (Open, Closed);
-   type OrbitHeight is range 350..450;
+   type OrbitHeight is range 0..2000;
    type Module is range 0..3;
    type ModuleRange is range 1..20;
    type ModuleStack is array (ModuleRange) of Module;
    type Move is (Left, Right);
    
+   MINIMUM_HEIGHT : constant OrbitHeight := 350;
+   MAXIMUM_HEIGHT : constant OrbitHeight := 450;
+   
    type MonitoringStation is record
       airLock1 : AirLockStatus := Closed;
       airLock2 : AirLockStatus := Closed;
       height : OrbitHeight := 400;
-      modules : ModuleStack; -- can you initialise the array values?
+      modules : ModuleStack;
       modulesCount : ModuleRange := ModuleRange'First;
    end record;
       
    solaris : MonitoringStation;
    
-   function AirLockInvariant return Boolean is
+   function AirlockInvariant return Boolean is
      (solaris.airLock1 = Closed or solaris.airLock2 = Closed);
    
    function OrbitHeightInvariant return Boolean is
-      (solaris.height >= OrbitHeight'First and solaris.height <= OrbitHeight'Last);
+      (solaris.height >= MINIMUM_HEIGHT and solaris.height <= MAXIMUM_HEIGHT);
    
-   function ModuleStackInvariant return Boolean is
-     (solaris.modulesCount >= ModuleRange'First and solaris.modulesCount <= ModuleRange'Last);
-
    
    -- air lock stuff
    procedure openAirLock1 with
      Global => (In_Out => solaris),
      Pre => solaris.airLock2 = Closed 
-     and AirLockInvariant
-     and OrbitHeightInvariant
-     and ModuleStackInvariant,
+     and AirlockInvariant
+     and OrbitHeightInvariant,
      Post => solaris.airLock1 = Open
      and solaris.airLock2 = Closed
-     and AirLockInvariant 
-     and OrbitHeightInvariant
-     and ModuleStackInvariant;
+     and AirlockInvariant 
+     and OrbitHeightInvariant;
    
    procedure openAirLock2 with
      Global => (In_Out => solaris),
      Pre => solaris.airlock1 = Closed 
-     and AirLockInvariant
-     and OrbitHeightInvariant
-     and ModuleStackInvariant,
+     and AirlockInvariant
+     and OrbitHeightInvariant,
      Post => solaris.airLock2 = Open
      and solaris.airLock1 = Closed
-     and AirLockInvariant 
-     and OrbitHeightInvariant
-     and ModuleStackInvariant;
+     and AirlockInvariant 
+     and OrbitHeightInvariant;
    
    procedure closeAirLock1 with
      Global => (In_Out => solaris),
-     Pre => AirLockInvariant 
-     and OrbitHeightInvariant
-     and ModuleStackInvariant,
+     Pre => AirlockInvariant 
+     and OrbitHeightInvariant,
      Post => solaris.airLock1 = Closed 
-     and AirLockInvariant
-     and OrbitHeightInvariant
-     and ModuleStackInvariant;
+     and AirlockInvariant
+     and OrbitHeightInvariant;
   
    procedure closeAirLock2 with
      Global => (In_Out => solaris),
-     Pre => AirLockInvariant 
-     and OrbitHeightInvariant
-     and ModuleStackInvariant,
+     Pre => AirlockInvariant 
+     and OrbitHeightInvariant,
      Post => solaris.airLock2 = Closed 
-     and AirLockInvariant
-     and OrbitHeightInvariant
-     and ModuleStackInvariant;
+     and AirlockInvariant
+     and OrbitHeightInvariant;
    
    
    -- orbit height stuff
    procedure increaseHeight with
      Global => (In_Out => solaris),
-     Pre => solaris.height < OrbitHeight'Last
-     and AirLockInvariant
-     and OrbitHeightInvariant
-     and ModuleStackInvariant,
-     Post => solaris.height <= OrbitHeight'Last
-     and solaris.height = solaris.height'Old + 1
-     and AirLockInvariant
-     and OrbitHeightInvariant
-     and ModuleStackInvariant;
+     Pre => solaris.height < MAXIMUM_HEIGHT
+     and AirlockInvariant
+     and OrbitHeightInvariant,
+     Post => solaris.height = solaris.height'Old + 1
+     and AirlockInvariant
+     and OrbitHeightInvariant;
    
    procedure decreaseHeight with 
      Global => (In_Out => solaris),
-     Pre => solaris.height > OrbitHeight'First
-     and AirLockInvariant
-     and OrbitHeightInvariant
-     and ModuleStackInvariant,
-     Post => solaris.height >= OrbitHeight'First
-     and solaris.height = solaris.height'Old - 1
-     and AirLockInvariant
-     and OrbitHeightInvariant
-     and ModuleStackInvariant;
+     Pre => solaris.height > MINIMUM_HEIGHT
+     and AirlockInvariant
+     and OrbitHeightInvariant,
+     Post => solaris.height = solaris.height'Old - 1
+     and AirlockInvariant
+     and OrbitHeightInvariant;
    
    
    -- module stuff
-   function Empty return Boolean is (solaris.modulesCount = ModuleRange'First);
+   function LastModule return Boolean is (solaris.modulesCount = ModuleRange'First);
 
    function Full return Boolean is (solaris.modulesCount = ModuleRange'Last);
 
@@ -108,23 +94,19 @@ package SpaceStation with SPARK_Mode is
      Global => (In_Out => solaris),
      Pre => not Full
      and AirLockInvariant
-     and OrbitHeightInvariant
-     and ModuleStackInvariant,
+     and OrbitHeightInvariant,
      Post => solaris.modulesCount = solaris.modulesCount'Old + 1
      and AirLockInvariant
-     and OrbitHeightInvariant
-     and ModuleStackInvariant;
+     and OrbitHeightInvariant;
 
    procedure popModule with
      Global => (In_Out => solaris),
-     Pre => not Empty and solaris.modules(solaris.modulesCount) = 0
-     and AirLockInvariant
-     and OrbitHeightInvariant
-     and ModuleStackInvariant,
+     Pre => not LastModule and solaris.modules(solaris.modulesCount) = 0
+     and AirlockInvariant
+     and OrbitHeightInvariant,
      Post => solaris.modulesCount = solaris.modulesCount'Old - 1
-     and AirLockInvariant
-     and OrbitHeightInvariant
-     and ModuleStackInvariant;
+     and AirlockInvariant
+     and OrbitHeightInvariant;
        
    procedure printStation;
    
@@ -137,15 +119,14 @@ package SpaceStation with SPARK_Mode is
      Pre => solaris.modules(moduleIndex) > 0 
      and (not (moduleIndex = 0) and moveDir = Left)
      and (not (moduleIndex = 20) and moveDir = Right)
-     and AirLockInvariant
-     and OrbitHeightInvariant
-     and ModuleStackInvariant,
-     Post => 
-       (solaris.modules(moduleIndex + 1) = solaris.modules(moduleIndex + 1)'Old + 1 or
-          solaris.modules(moduleIndex -1) = solaris.modules(moduleIndex -1)'Old + 1)
-     and AirLockInvariant
-     and OrbitHeightInvariant
-     and ModuleStackInvariant;
+     and AirlockInvariant
+     and OrbitHeightInvariant,
+     Post => solaris.modules(moduleIndex) = solaris.modules(moduleIndex)'Old - 1
+     and ((moveDir = Right and solaris.modules(moduleIndex + 1) = solaris.modules(moduleIndex + 1)'Old + 1)
+          or
+           (moveDir = Left and solaris.modules(moduleIndex -1) = solaris.modules(moduleIndex -1)'Old + 1))
+     and AirlockInvariant
+     and OrbitHeightInvariant;
    
    
 end SpaceStation;
